65969ae4e5f42b2bbe0915fbd22de887b556274f
许浩培 <986119817@qq.com>
2020-05-23 20:37:56 +0800

    添加了user接口

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 
 // 引入第三方插件
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // 引入自己写的API接口
 const apiUser = require('./routers/user')
@@ -10,10 +11,11 @@ const apiUser = require('./routers/user')
 // 使用第三方插件
 app.use(bodyParser.json());                             // 当post请求时，里面的参数可以通过req.body()获取一个对象
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());                                        // 当请求跨域时，允许获取资源
 
 // 使用API接口
 app.use(apiUser);
 
 app.listen(3000,()=>{
-    console.log("启动服务: http://127.0.0.1:3000")
+    console.log("启动服务: localhost:3000")
 });
--- routers/user.js
+++ routers/user.js
@@ -2,16 +2,23 @@ const epxress = require('express');
 const pool = require('../sql/pool');
 const router = epxress.Router();
 
-router.get('/user',(res,req,next)=>{
-    pool.hp_query("select * from user",(err,data)=>{
+router.get('/user/query',(res,req,next)=>{
+    pool.hp_query('select * from user',(err,data)=>{
         if(err) next(err)
         req.json(data);
     })
 })
 
+router.post('/user/add',(req,res,next)=>{
+    pool.hp_query(`INSERT INTO user (userName,email,password) VALUES ('${req.body.username}','${req.body.email}','${req.body.password}')`,(err,data)=>{
+        if(err) next(err)
+        res.json(data);
+    })
+})
 // 当调用api出现错误时，统一到这里处理;
 router.use((err,req,res,next)=>{
     if(err) {
+        console.log(err);
         res.status(500).json(err); 
     }
 })
