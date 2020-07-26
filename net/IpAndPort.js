6325c906ad436d86625b50fbbcf039768496f602
许浩培 <986119817@qq.com>
2020-07-26 16:02:46 +0800

    端口bug

--- net/IpAndPort.js
+++ net/IpAndPort.js
@@ -1,6 +1,8 @@
 const os = require('os');
-const port = 3000;
+//const port = 80;                                        // 本地window
 //const ip = os.networkInterfaces().WLAN[1].address;      // 本地window
+
+const port = 80;                                          // 服务器
 const ip = '116.85.46.202'                                // 服务器
 exports.port = port;
 exports.ip = ip;
