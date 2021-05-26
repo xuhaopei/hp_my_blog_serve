d1da624869ba0803f1d43c39bef2ab474c140813
许浩培 <986119817@qq.com>
2021-05-26 18:29:45 +0800

    修复ip

--- net/IpAndPort.js
+++ net/IpAndPort.js
@@ -1,8 +1,8 @@
-const port = 5200;                                           // 本地window
-const ip = 'localhost';                                      // 本地window
+// const port = 5200;                                           // 本地window
+// const ip = 'localhost';                                      // 本地window
 
-// const port = 80;                                          // 服务器
-// const ip = '106.75.167.111'                                // 服务器
+const port = 5200;                                          // 服务器
+const ip = '106.75.167.111'                                // 服务器
 
 
 exports.port = port;
